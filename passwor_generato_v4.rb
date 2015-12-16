801826f5b1ca53de489f7e07466655fb60241dce
Alex <sasha-erem@mail.ru>
2015-12-16 19:47:14 +0400

    password_generator_v4
    password_generator_v4

---
+++ passwor_generato_v4.rb
@@ -0,0 +1,30 @@
+class Passworgenerator
+
+  ALPHABET=('a'..'z').to_a
+  VOWELS=%w(a e o i y u)
+  CONSONANTS=ALPHABET-VOWELS
+
+  def initialize
+
+    @password = Array.new
+
+  end
+
+  def generation(size)
+
+
+    vowel = [true,false].sample
+
+    size.times do |x|
+        
+      @password << Passworgenerator::VOWELS.sample if vowel
+      @password << Passworgenerator::CONSONANTS.sample unless vowel
+      vowel = !vowel
+
+    end
+
+  end
+
+end
+
+
